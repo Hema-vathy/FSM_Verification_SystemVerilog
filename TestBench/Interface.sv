d7d099c46635b4dd4c2d523f736d4435b39abb73
Hema-vathy <158808035+Hema-vathy@users.noreply.github.com>
2025-10-31 17:47:29 +0530

    Add interface

---
+++ TestBench/Interface.sv
@@ -0,0 +1,8 @@
+//////////////*************INTERFACE FILE - CONTAIN ALL THE SIGNAL NEEDED FOR THE VERIFICATION*************//////////////
+interface signal;
+  logic clock;
+  logic reset;
+  logic sequence_in;
+  logic detector_out;
+  logic sequence_q[$];
+endinterface
