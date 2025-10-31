5bb05434cbf1f54df259dda471678ca9e37878d6
Hema-vathy <158808035+Hema-vathy@users.noreply.github.com>
2025-10-31 17:49:12 +0530

    Add package containing testbench files

---
+++ TestBench/Files.sv
@@ -0,0 +1,9 @@
+/////////////*************PACKAGE CONTAINING FILES*************/////////////
+package pkg;
+`include "clk_gen.sv"
+`include "stimuli.sv"
+`include "rst_gen.sv"
+`include "reset_test.sv"
+`include "comparison_test.sv"
+`include "run_test.sv"
+endpackage
